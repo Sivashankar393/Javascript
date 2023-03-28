1bd38b505d95f51f9ae76e3633606ab747761908
Sivashankar393 <Sivashankar393@gmail.com>
2023-03-28 23:32:41 +0530

    ok

---
+++ Test3.js
@@ -0,0 +1,5 @@
+//5 to 15 numbers Using For Loop
+let a=5
+for( a=5; a<=15; a++){
+    console.log(a)
+}
---
+++ Test7.js
@@ -0,0 +1,6 @@
+//first 10 multiples of 4 using while loop
+var a=1;
+while( a<=10 ){
+        console.log(a*4);
+    a++;
+}
