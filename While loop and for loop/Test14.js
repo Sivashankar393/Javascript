7089ac4139b9c50536eae8717bc8e23737a34870
Sivashankar393 <Sivashankar393@gmail.com>
2023-03-29 13:30:33 +0530

    ok

---
+++ While loop and for loop/Test14.js
@@ -0,0 +1,10 @@
+// program to generate fibonacci series up to n terms
+let one =0;
+let two =1;
+for (let i = 1; i<=10;i++);{
+    let current = one +two
+    one = two;
+    two = current;
+    console.log(current);
+}
+    
